2a92db433c07b2d235822073e141c2ec7119e700
Vishnu Sresht <vishnu.sresht@pfizer.com>
2018-08-10 20:19:02 -0400

    Add script to run training with property prediction

--- mol_vae/pytorch_train/run_train_prop.sh
+++ mol_vae/pytorch_train/run_train_prop.sh
@@ -4,9 +4,10 @@ dropbox=../../dropbox
 
 grammar_file=$dropbox/context_free_grammars/mol_zinc.grammar
 sk=0
-#data_dump=$dropbox/data/zinc/250k_rndm_zinc_drugs_clean-${sk}.h5
-data_dump=$dropbox/data/mattrack_avail/mattrack_mostly_good_subset_1260K-0.h5
-prop_file=$dropbox/data/mattrack_avail/mattrack_mostly_good_subset_1260K-0_xlogp.csv
+data_dump=$dropbox/data/zinc/250k_rndm_zinc_drugs_clean-${sk}.h5
+prop_file=$dropbox/data/zinc/250k_rndm_zinc_drugs_clean-clogp-${sk}.csv
+#data_dump=$dropbox/data/mattrack_avail/mattrack_mostly_good_subset_1260K-0.h5
+#prop_file=$dropbox/data/mattrack_avail/mattrack_mostly_good_subset_1260K-0_xlogp.csv
 #data_dump=$dropbox/data/mattrack_avail/10_glp1_plus_mattrack-${sk}.h5
 #data_dump=$dropbox/data/mattrack_avail/05_glp1_plus_mattrack-${sk}.h5
 #data_dump=$dropbox/data/glp1/glp1_smiles-0.h5
@@ -18,11 +19,12 @@ ae_type=vae
 loss_type=vanilla
 rnn_type=gru
 kl_coeff=1
+prop_coeff=1
 lr=0.0001
-num_epochs=100
+num_epochs=500
 eps_std=0.01
 prob_fix=0
-save_dir=/localscratch/sdvae_results/graph_generation/mattrack_prop_01_vanilla-sk-${sk}-mol_${ae_type}/enc-${enc}-loss-${loss_type}-eps-${eps_std}-rnn-${rnn_type}-kl-${kl_coeff}
+save_dir=/localscratch/sdvae_results/graph_generation/zink_prop_00_vanilla-sk-${sk}-mol_${ae_type}/enc-${enc}-loss-${loss_type}-eps-${eps_std}-rnn-${rnn_type}-kl-${kl_coeff}
 
 if [ ! -e $save_dir ];
 then
@@ -45,5 +47,6 @@ python train_zinc_prop.py \
     -eps_std $eps_std \
     -loss_type $loss_type \
     -kl_coeff $kl_coeff \
+    -prop_coeff $prop_coeff \
     -prob_fix $prob_fix \
     -mode gpu
